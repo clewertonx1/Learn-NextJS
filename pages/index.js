fb57ee423952d0a1f23e2f8efc256ef477ff2e93
Clewerton Roque <clewerton203@gmail.com>
2020-11-20 02:38:20 -0300

    Teste

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,23 @@
+
+const style = {
+   
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+    
+}
 function Home(){
-    return <div>Home</div>
+    return <html>
+        <body style={style}> 
+            <div>
+                <h1 style={{color: 'grey', fontFamily: 'Arial'}}>Eae Nicole,</h1>
+                <h1 style={{color: 'grey', fontFamily: 'Arial'}}>Eu to testando uma nova tecnologia e olha que massa</h1>
+                <h1 style={{color: 'grey', fontFamily: 'Arial'}}>Isso ta hospeado na internet de forma gratuita, cara isso é doido</h1>
+                <h1 style={{color: 'grey', fontFamily: 'Arial'}}>O nome dessa tecnologia é <span style={{color: 'green'}}>NextJS</span></h1>
+                <h2 style={{color: 'grey', fontFamily: 'Arial'}}>Caralho a que ponto a tecnologia chegou</h2>
+                <h3 style={{color: 'red', fontFamily: 'Arial'}}>Hey, eu te amo ❤️</h3>
+            </div>
+        </body>
+    </html>
 }
 export default Home
